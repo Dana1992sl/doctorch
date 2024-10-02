2578c55c5207991e67fa9924a3867f2a014ec4d6
Dana1992sl <danamansl123@gmail.com>
2024-10-02 21:47:43 +0530

    navbar1

--- src/NavBar/Navbar.jsx
+++ src/NavBar/Navbar.jsx
@@ -2,19 +2,28 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const Navbar = () => {
+
   return (
     <div className='navbar'>
-    <nav>
+    <div className='logo'>
     <img src={assets.logo} alt='logo' className='logo' />
-    <ul className='nav-links'>
+    </div>
+    <nav>
+    
+
+    <div className='nav-links'>
+    <ul >
         <li><a>Home</a></li>
         <li><a>About</a></li>
-
+        <li><a>Contact</a></li>
     </ul>
-
-
+    </div>
 
     </nav>
+    <div className='search-bar'>
+            <input type='text' placeholder='Search' />
+            <button>Search</button>
+        </div>
       
     </div>
   )
